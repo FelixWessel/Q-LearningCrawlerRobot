ca5d0b692b1c1932291e57ce81c6a6a37fbe5e36
FelixWessel <felix.wessel@googlemail.com>
2021-07-28 18:01:44 +0200

    Updated Constructor

--- Servo.py
+++ Servo.py
@@ -1,3 +1,10 @@
 class Servo:
-    def __init__(self):
-        
+    def __init__(self, connection, numberOfStates, initialAngle, minAngle, maxAngle):
+        # hier muss noch der Anschluss aus der SuperKlasse rein
+        self.numberOfStates = numberOfStates
+        self.initialAngle = initialAngle
+        self.currentAngle = self.initialAngle
+        self.minAngle = minAngle
+        self.maxAngle = maxAngle
+        self.stepAngle = int((self.maxAngle-self.minAngle)/(self.numberOfStates-1))
+        self.initialServoState=int(((self.initialAngle-self.minAngle)/self.stepAngle)) #This is an integer between zero and numOfStates-1 used to index the state number of servo
