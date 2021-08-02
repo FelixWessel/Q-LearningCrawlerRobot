2dd4014009c02e68e6b8ff545895a587d3526205
Felix Wessel <felix.wessel@googlemail.com>
2021-08-02 18:06:21 +0200

    New file for HC-SR04 Sensor

---
+++ HC-SR04.py
@@ -0,0 +1,48 @@
+#Import Libraries
+import RPi.GPIO as GPIO
+import time
+ 
+#Define GPIO (Mode, Pins, Output)
+GPIO.setmode(GPIO.BCM)
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+ 
+def read():
+    # Set Trig High
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # Set Trig Low (after 0.01ms)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    startTime = time.time()
+    endTime = time.time()
+ 
+    # Calculate Start and End Time
+    while GPIO.input(GPIO_ECHO) == 0:
+        startTime = time.time()
+ 
+    while GPIO.input(GPIO_ECHO) == 1:
+        endTime = time.time()
+ 
+    # Calculate Time Difference
+    differenceTime = endTime - startTime
+	
+    # Speed of Sound (34300 cm/s) 
+    distance = (differenceTime * 34300) / 2
+ 
+    return distance
+#  
+# if __name__ == '__main__':
+#     try:
+#         while True:
+#             distanz = entfernung()
+#             print ("Distanz = %.1f cm" % distanz)
+#             time.sleep(1)
+#  
+#         # Programm beenden
+#     except KeyboardInterrupt:
+#         print("Programm abgebrochen")
+#         GPIO.cleanup()
